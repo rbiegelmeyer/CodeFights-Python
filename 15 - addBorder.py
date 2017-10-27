11fa2895d08e169d83f6fc342d9d4c568fb27cb8
Roberto <robertobiegel@gmail.com>
2017-10-27 21:26:05 -0200

    [UPD] Melhoria no 15

--- 15 - addBorder.py
+++ 15 - addBorder.py
@@ -1,15 +1,10 @@
 def addBorder(picture):
     matrix = []
-    borderSupInf = ""
-
-    for i in range(len(picture[0])+2):
-        borderSupInf += "*"
-
+    borderSupInf = (len(picture[0])+2)*"*"
     matrix.append(borderSupInf)
+
     for x in picture:
-         matrix.append("*" + x + "*")
+         matrix.append("*" + x + "*") 
     matrix.append(borderSupInf)
-
+    
     return matrix
-        
-print (addBorder(["a"]))
