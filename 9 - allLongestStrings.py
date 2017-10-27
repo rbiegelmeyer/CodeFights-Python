0d9a6a2e4f300a07fd71f68cdf275b335076ea28
rbiegelmeyer <robertobiegel@gmail.com>
2017-10-27 21:29:31 -0200

    [UPD] Melhoria no 9

--- 9 - allLongestStrings.py
+++ 9 - allLongestStrings.py
@@ -1,3 +1,5 @@
 def allLongestStrings(inputArray):
-    	size = max(len(s) for s in inputArray)
-	return [x for x in inputArray if len(x) == size]
+    size = max(len(s) for s in inputArray)
+    return [x for x in inputArray if len(x) == size]
+
+print allLongestStrings(["aaa","dsds","dsfs"])
