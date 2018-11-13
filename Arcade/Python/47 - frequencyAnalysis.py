8b50a4663a765fab19ec8ebc6ebb81b4f6207d40
rbiegelmeyer <robertobiegel@gmail.com>
2018-11-13 21:06:42 -0200

    [ADD] Python 47

---
+++ Arcade/Python/47 - frequencyAnalysis.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def frequencyAnalysis(encryptedText):
+    return Counter(encryptedText).most_common(1)[0][0]
+
+
+encryptedText = "$~NmiNmim$/NVeirp@dlzrCCCCfFfQQQ"
+
+print (frequencyAnalysis(encryptedText))
+
+# "C"
